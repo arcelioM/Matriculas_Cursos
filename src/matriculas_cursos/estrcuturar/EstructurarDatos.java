b0fdefd9b7bc6b2f5d78fab95c24e2e09fd57ba9
arcelioM <arceliomonte15@gmail.com>
2022-10-22 14:42:16 -0500

    Metodo para estructurar y  mostrar listado de matricula y cursos agregado

--- src/matriculas_cursos/estrcuturar/EstructurarDatos.java
+++ src/matriculas_cursos/estrcuturar/EstructurarDatos.java
@@ -4,9 +4,13 @@ package matriculas_cursos.estrcuturar;
 import java.util.List;
 import java.util.Vector;
 import javax.swing.table.DefaultTableModel;
+import matriculas_cursos.dao.CursoMatriculaDao;
+import matriculas_cursos.dao.CursosDao;
 import matriculas_cursos.dao.EstudianteDao;
 import matriculas_cursos.dao.MatriculasDao;
 import matriculas_cursos.dao.TurnosDao;
+import matriculas_cursos.model.CursoMatricula;
+import matriculas_cursos.model.Cursos;
 import matriculas_cursos.model.Estudiante;
 import matriculas_cursos.model.Matriculas;
 import matriculas_cursos.model.Turnos;
@@ -17,6 +21,8 @@ public class EstructurarDatos {
     private final MatriculasDao matriculaDao = new MatriculasDao();
     private final TurnosDao turnosDao = new TurnosDao();
     private final EstudianteDao estudianteDao = new EstudianteDao();
+    private final CursoMatriculaDao cursoMatriculaDao= new CursoMatriculaDao();
+    private final CursosDao cursosDao = new CursosDao();
     
     public DefaultTableModel cargarDatoMatriculas(){
         DefaultTableModel tableModel= new DefaultTableModel();
@@ -59,5 +65,34 @@ public class EstructurarDatos {
         }
        
        return tableModel;
+    }
+    
+    public DefaultTableModel cargarDatosCursoPorMatricula(Integer idMatricula){
+        Matriculas matricula = new Matriculas(idMatricula);
+        CursoMatricula cursoMatricula = new CursoMatricula(matricula);
+        List<CursoMatricula> matriculaCursos=cursoMatriculaDao.getByMatriculaId(cursoMatricula);
+        
+        DefaultTableModel tableModel = new DefaultTableModel();
+        tableModel.addColumn("ID");
+        tableModel.addColumn("Nombre");
+        
+        for(CursoMatricula cm : matriculaCursos){
+            Cursos curso = new Cursos(cm.getCursoId().getId());
+            curso=cursosDao.getById(curso);
+            cm.setCursoId(curso);
+        }
+        
+        for (CursoMatricula cm : matriculaCursos){
+            Vector<Object> row = new Vector<>();
+            Integer idCurso = cm.getCursoId().getId();
+            row.add(idCurso);
+            
+            String nombreCurso = cm.getCursoId().getNombre();
+            row.add(nombreCurso);
+            
+            tableModel.addRow(row);
+        }
+        
+        return tableModel;
     }
 }
