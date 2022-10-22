b349a307507bdef92699c4f39f8919f61f5b12a3
arcelioM <arceliomonte15@gmail.com>
2022-10-21 19:22:59 -0500

    Revert "Cambios hechos en conexion de BD"
    This reverts commit e4694c56612ac5cc6a24d5317c57b41ea6700cf9.

--- src/matriculas_cursos/dao/connection/ConnectionMySql.java
+++ src/matriculas_cursos/dao/connection/ConnectionMySql.java
@@ -7,56 +7,21 @@ import java.sql.SQLException;
 
 public class ConnectionMySql {
     
-    private static ConnectionMySql CONEXION;
-    static Connection conn;
+    private static final String USERNAME="root";
+    private static final String PASS="holaCOMO";
+    private static final String  HOST="localhost";
+    private static final String DATABASE="matriculas";
+    private static final String PORT="3306";
 
-    private ConnectionMySql() {
-        this.crearConexionBd();
-    }
-
-    private synchronized  static void crearInstancia(){
-        if(ConnectionMySql.CONEXION==null){
-            ConnectionMySql.CONEXION= new ConnectionMySql();
-        }
-    }
-    
-    public static ConnectionMySql getInstance(){
-        if(ConnectionMySql.CONEXION==null){
-            ConnectionMySql.crearInstancia();
-        }
-        return ConnectionMySql.CONEXION;
-    }
-    
-    public void crearConexionBd(){
+    public static Connection getConexion(){
         
-        String usarname="root";
-        String  host="localhost";
-        String pass="holaCOMO";
-        String database="matriculas";
-        String port="3306";
-        
-         try{
-            ConnectionMySql.conn=DriverManager.getConnection("jdbc:mysql://"+host+":"+port+"/"+database, usarname, pass);
-            ConnectionMySql.conn.setAutoCommit(false);
-        }catch(SQLException e){
-             System.out.println("Error en creacionde BD");
-        } 
-        
-    }
-    
-    public static void cerrarConexion(){
         try{
-            ConnectionMySql.conn.close();
+            Connection conn=DriverManager.getConnection("jdbc:mysql://"+HOST+":"+PORT+"/"+DATABASE, USERNAME, PASS);
+           return conn;
         }catch(SQLException e){
-            System.out.println("Error al cerrar conexion");
-        }
+            e.printStackTrace(System.out);
+            return null;
+        } 
     }
-    
-    
-    public static void delInstance(){
-        ConnectionMySql.CONEXION=null;
-        ConnectionMySql.cerrarConexion();
-    }
-    
     
 }
